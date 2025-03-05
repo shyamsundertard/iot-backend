72eb921f53fdb62e91959321bb8eef53b708191b
Shyamsunder Tard <iamshyamsundertard@gmail.com>
2025-03-05 23:39:29 +0530

    Included CORS details

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ import mpuRoutes from './routes/mpuSensor.js';
 import mpu6050Routes from "./routes/mpu6050.js"
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // Routes
